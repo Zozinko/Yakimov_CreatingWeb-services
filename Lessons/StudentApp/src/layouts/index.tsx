7ef93476e29457441862c736819cfda67f8a9c08
Zozinko <kolyamr16@gmail.com>
2026-03-01 21:39:14 +0500

    Lesson_1

--- Lessons/StudentApp/src/layouts/index.tsx
+++ Lessons/StudentApp/src/layouts/index.tsx
@@ -1,21 +1,87 @@
 import { Link, Outlet } from 'umi';
 import styles from './index.less';
 
-export default function Layout() {
-  return (
-    <div className={styles.navs}>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/docs">Docs</Link>
-        </li>
-        <li>
-          <a href="https://github.com/umijs/umi">Github</a>
-        </li>
-      </ul>
-      <Outlet />
-    </div>
-  );
+import React, { useState } from 'react';
+
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons';
+import { Button, Layout, Menu, theme } from 'antd';
+
+const { Header, Sider, Content } = Layout;
+
+const App: React.FC = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+
+    return (
+      // <div className={styles.navs}>
+      //   <ul>
+      //     <li>
+      //       <Link to="/">Home</Link>
+      //     </li>
+      //     <li>
+      //       <Link to="/docs">Docs</Link>
+      //     </li>
+      //     <li>
+      //       <a href="https://github.com/umijs/umi">Github</a>
+      //     </li>
+      //   </ul>
+      //   <Outlet />
+      // </div>
+          <Layout>
+        <Sider trigger={null} collapsible collapsed={collapsed}>
+          <div className="demo-logo-vertical" />
+          <Menu
+            theme="dark"
+            mode="inline"
+            defaultSelectedKeys={['1']}
+            items={[
+              {
+                key: 'home',
+                icon: <UserOutlined />,
+                label: <Link to="/">Home</Link>,
+              },
+              {
+                key: 'dock',
+                icon: <UserOutlined />,
+                label: <Link to="/docs">About App</Link>,
+              },
+            ]}
+          />
+        </Sider>
+        <Layout>
+          <Header style={{ padding: 0, background: colorBgContainer }}>
+            <Button
+              type="text"
+              icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+              onClick={() => setCollapsed(!collapsed)}
+              style={{
+                fontSize: '16px',
+                width: 64,
+                height: 64,
+              }}
+            />
+          </Header>
+          <Content
+            style={{
+              margin: '24px 16px',
+              padding: 24,
+              minHeight: 280,
+              background: colorBgContainer,
+              borderRadius: borderRadiusLG,
+            }}
+          >
+          <Outlet />
+          </Content>
+        </Layout>
+      </Layout>
+    );
 }
+export default App;
--- Lessons/StudentApp/src/pages/docs.tsx
+++ Lessons/StudentApp/src/pages/docs.tsx
@@ -1,9 +1,40 @@
-const DocsPage = () => {
+import React, { useState } from 'react';
+import { Button, Modal } from 'antd';
+import yayJpg from '../assets/notYay.png';
+
+const Lesson_1: React.FC = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
   return (
-    <div>
-      <p>This is umi docs.</p>
-    </div>
+    <>
+      <h2>About</h2>
+      
+      <Button type="primary" onClick={showModal}>
+        Нажми пожалуйста
+      </Button>
+      <Modal
+        title="Фотография разработчика"
+        closable={{ 'aria-label': 'Custom Close Button' }}
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}  
+      >
+        <img src={yayJpg} width="388" />
+      </Modal>
+    </>
   );
 };
 
-export default DocsPage;
+export default Lesson_1;
