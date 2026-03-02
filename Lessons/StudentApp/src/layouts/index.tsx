cfd681cb9039711a3c792dcd747e161979333ca5
Zozinko <kolyamr16@gmail.com>
2026-03-02 16:30:00 +0500

    Лекция 2

--- Lessons/StudentApp/src/layouts/index.tsx
+++ Lessons/StudentApp/src/layouts/index.tsx
@@ -1,21 +1,48 @@
 import { Link, Outlet } from 'umi';
 import styles from './index.less';
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import { lazy } from 'react';
+import SuperMan from '@/components/SuperMan';
 
-export default function Layout() {
+const { Header, Content, Footer } = Layout;
+
+const items = [
+  {
+    key: 'home',
+    label: <Link to = "/">Home</Link>
+  },
+  {
+    key: 'docs',
+    label: <Link to = "/docs">Docs</Link>
+  },
+]
+
+
+export default function() {
   return (
-    <div className={styles.navs}>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/docs">Docs</Link>
-        </li>
-        <li>
-          <a href="https://github.com/umijs/umi">Github</a>
-        </li>
-      </ul>
-      <Outlet />
-    </div>
+    <Layout>
+      <Header style={{ display: 'flex', alignItems: 'center' }}>
+        <div className="demo-logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['2']}
+          items={items}
+          style={{ flex: 1, minWidth: 0 }}
+        />
+      </Header>
+      <Content style={{ padding: '0 48px' }}>
+        <Breadcrumb
+          style={{ margin: '16px 0' }}
+          items={[{ title: 'Home' }, { title: 'List' }, { title: 'App' }]}
+        />
+        <div>
+          <Outlet />
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>
+        Ant Design ©{new Date().getFullYear()} Created by Ant UED
+      </Footer>
+    </Layout>
   );
 }
--- Lessons/StudentApp/src/pages/index.tsx
+++ Lessons/StudentApp/src/pages/index.tsx
@@ -1,8 +1,7 @@
 import yayJpg from '../assets/yay.jpg';
-
 export default function HomePage() {
   return (
-    <div>
+    <div>    
       <h2>Yay! Welcome to umi!</h2>
       <p>
         <img src={yayJpg} width="388" />
